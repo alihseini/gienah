e24084ca0cfa8b4f10f55f5a44bba128fe12bd2d
Ali Hosseini <a.hosseini9@outlook.com>
2026-07-08 10:23:54 +0330

    try to remove the under page of safari

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,10 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   viewportFit: "cover",
-  themeColor: "#050b14",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#050b14" },
+    { media: "(prefers-color-scheme: dark)", color: "#050b14" },
+  ],
   colorScheme: "dark",
 };
 
